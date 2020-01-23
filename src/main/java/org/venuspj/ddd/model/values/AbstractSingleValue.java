83acfa1873e5ca73ee9aeeeb7c1ea57c12868173
mizoguchikenichi <hachirouuemon.mizoguchi@gmail.com>
2020-01-24 08:08:37 +0900

    add EnumCriteria

---
+++ src/main/java/org/venuspj/ddd/model/values/AbstractSingleValue.java
@@ -0,0 +1,65 @@
+package org.venuspj.ddd.model.values;
+
+import org.venuspj.ddd.model.values.primitives.SingleValue;
+
+import javax.annotation.Nullable;
+
+import static org.venuspj.util.objects2.Objects2.isNull;
+import static org.venuspj.util.objects2.Objects2.toStringHelper;
+
+public abstract class AbstractSingleValue<T, ASV extends AbstractSingleValue<T, ASV>> implements SingleValue<T, ASV>, Comparable<ASV> {
+
+    protected T value;
+
+    public AbstractSingleValue() {
+
+    }
+
+    protected AbstractSingleValue(T aValue) {
+        value = aValue;
+
+    }
+
+    @Override
+    public T getValue() {
+        return value;
+    }
+
+    public boolean sameValueAs(ASV that) {
+        if (isNull(that))
+            return false;
+
+        return org.venuspj.util.objects2.Objects2.equal(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return org.venuspj.util.objects2.Objects2.hash(value);
+
+    }
+
+    public String asText() {
+        if (isNull(value))
+            return null;
+        return value.toString();
+    }
+
+    @Override
+    public boolean equals(@Nullable Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ASV that = (ASV) o;
+        return sameValueAs(that);
+    }
+
+    @Override
+    public abstract int compareTo(@Nullable ASV o);
+
+    @Override
+    public String toString() {
+        return toStringHelper(this)
+                .defaultConfig()
+                .toString();
+
+    }
+}
