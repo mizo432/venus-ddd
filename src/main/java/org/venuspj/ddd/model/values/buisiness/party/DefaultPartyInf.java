b06cd8f620c0812597b7a9f9d32063e74b008dc7
mizoguchikenichi <hachirouuemon.mizoguchi@gmail.com>
2020-01-25 17:06:05 +0900

    コンパイルエラーを解消した。

--- src/main/java/org/venuspj/ddd/model/values/buisiness/party/DefaultPartyInf.java
+++
@@ -1,7 +0,0 @@
-package org.venuspj.ddd.model.values.buisiness.party;
-
-public class DefaultPartyInf extends AbstractPartyInf<DefaultPartyInf> {
-    public static DefaultPartyInf empty() {
-        return new DefaultPartyInf();
-    }
-}
--- src/main/java/org/venuspj/ddd/model/values/buisiness/party/Party.java
+++ src/main/java/org/venuspj/ddd/model/values/buisiness/party/Party.java
@@ -1,11 +1,21 @@
 package org.venuspj.ddd.model.values.buisiness.party;
 
 import org.venuspj.ddd.model.entity.AbstractEntity;
-import org.venuspj.ddd.model.values.buisiness.name.NamedObject;
 
 import static org.venuspj.util.objects2.Objects2.isNull;
 
-public class Party<P extends Party<P, PI, API, NO>, PI extends PartyIdentifier<P, PI>, API extends AbstractPartyInf<API, NO>, NO extends NamedObject<NO>> extends AbstractEntity<P, PI> {
+/**
+ * パーティー.
+ *
+ * @param <P>   パーティー
+ * @param <PI>  パーティーID
+ * @param <API> パーティー 情報
+ */
+public class Party<
+        P extends Party<P, PI, API>,
+        PI extends PartyIdentifier<P, PI>,
+        API extends AbstractPartyInf<API, ?>>
+        extends AbstractEntity<P, PI> {
     protected API partyInf;
 
     protected Party(PI aPartyIdentifier, API aPartyInf) {
