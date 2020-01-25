e82baf1de417343dab3ed3f754b2db546e6d6bcf
mizoguchikenichi <hachirouuemon.mizoguchi@gmail.com>
2020-01-25 18:36:44 +0900

    ref #3 add EnumCriteria

--- src/main/java/org/venuspj/ddd/model/repository/criteria/typeCriteria/EnumCriteria.java
+++ src/main/java/org/venuspj/ddd/model/repository/criteria/typeCriteria/EnumCriteria.java
@@ -2,19 +2,42 @@ package org.venuspj.ddd.model.repository.criteria.typeCriteria;
 
 import org.venuspj.ddd.model.repository.criteria.AbstractCriteria;
 import org.venuspj.ddd.model.repository.criteria.Criteria;
+import org.venuspj.ddd.model.repository.criteria.expressionCriteria.ExcludeCriteria;
+import org.venuspj.ddd.model.repository.criteria.expressionCriteria.IncludeCriteria;
 import org.venuspj.ddd.model.repository.criteria.expressionCriteria.IsEqualToCriteria;
 
 public class EnumCriteria<E extends Enum> extends AbstractCriteria<E> {
 
-    private IsEqualToCriteria<E> equalCriteria = new IsEqualToCriteria<>();
+    private IsEqualToCriteria<E> isEqualCriteria = new IsEqualToCriteria<>(this);
+    private IncludeCriteria<E> includeCriteria = new IncludeCriteria<>(this);
+    private ExcludeCriteria<E> excludeCriteria = new ExcludeCriteria<>(this);
 
     public EnumCriteria(Criteria<?> parent) {
         super(parent);
 
     }
 
+    public EnumCriteria() {
+
+    }
+
     @Override
     public boolean test(E value) {
-        return equalCriteria.test(value);
+        return isEqualCriteria.test(value)
+                && includeCriteria.test(value)
+                && excludeCriteria.test(value);
+    }
+
+    public IsEqualToCriteria<E> getIsEqualCriteria() {
+        return isEqualCriteria;
+
+    }
+
+    public IncludeCriteria<E> getIncludeCriteria() {
+        return includeCriteria;
+    }
+
+    public ExcludeCriteria<E> getExcludeCriteria() {
+        return excludeCriteria;
     }
 }
