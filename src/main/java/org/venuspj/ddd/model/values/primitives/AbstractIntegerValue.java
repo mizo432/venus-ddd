e55f9d5be34b2615334b42ed1ee7c4f431bc92fa
溝口顕一 <hachirouuemon.mizoguchi@gmail.com>
2020-02-02 17:53:35 +0900

    unit test を追加した

--- src/main/java/org/venuspj/ddd/model/values/primitives/AbstractIntegerValue.java
+++ src/main/java/org/venuspj/ddd/model/values/primitives/AbstractIntegerValue.java
@@ -3,6 +3,7 @@ package org.venuspj.ddd.model.values.primitives;
 import org.venuspj.ddd.model.values.AbstractComparableSingleValue;
 
 import static org.venuspj.util.objects2.Objects2.isNull;
+import static org.venuspj.util.objects2.Objects2.nonNull;
 
 public abstract class AbstractIntegerValue<AIV extends AbstractIntegerValue<AIV>>
         extends AbstractComparableSingleValue<Integer, AIV> implements IntegerValue<AIV> {
@@ -19,12 +20,35 @@ public abstract class AbstractIntegerValue<AIV extends AbstractIntegerValue<AIV>
     @Override
     public Integer asInteger() {
         return value;
+
     }
 
     @Override
     public String asText() {
         if (isNull(value)) return null;
         return value.toString();
+    }
+
+    public boolean isEmpty() {
+        return isNull(value);
+
+    }
+
+    public boolean isPresent() {
+        return nonNull(value);
+
+    }
+
+    public boolean isZero() {
+        return 0 == value;
+    }
+
+    public boolean isNegative() {
+        return 0 > value;
+    }
+
+    public boolean isPositive() {
+        return 0 < value;
     }
 
 }
