8ce06e133e47f34974b67eebac094b2635df2504
溝口顕一 <hachirouuemon.mizoguchi@gmail.com>
2020-02-05 22:49:12 +0900

    testを追加した

--- src/main/java/org/venuspj/ddd/model/values/buisiness/contact/Contact.java
+++ src/main/java/org/venuspj/ddd/model/values/buisiness/contact/Contact.java
@@ -1,19 +1,23 @@
 package org.venuspj.ddd.model.values.buisiness.contact;
 
 import org.venuspj.ddd.model.values.Value;
-import org.venuspj.ddd.model.values.buisiness.address.Address;
+import org.venuspj.ddd.model.values.buisiness.address.AddressInformation;
 import org.venuspj.ddd.model.values.buisiness.contact.email.EmailAddress;
 import org.venuspj.ddd.model.values.buisiness.contact.telephone.TelephoneNumber;
+import org.venuspj.util.builder.ObjectBuilder;
 import org.venuspj.util.objects2.Objects2;
 
+import static org.venuspj.util.objects2.Objects2.isNull;
+import static org.venuspj.util.objects2.Objects2.toStringHelper;
+
 public class Contact implements Value<Contact> {
-    private Address address = Address.empty();
+    private AddressInformation addressInformation = AddressInformation.empty();
     private EmailAddress emailAddress = EmailAddress.empty();
     private TelephoneNumber telephoneNumber = TelephoneNumber.empty();
     private TelephoneNumber faxNumber = TelephoneNumber.empty();
 
-    Contact(Address anAddress, EmailAddress anEmailAddress, TelephoneNumber aTelephoneNumber, TelephoneNumber aFaxNumber) {
-        address = anAddress;
+    Contact(AddressInformation anAddressInformation, EmailAddress anEmailAddress, TelephoneNumber aTelephoneNumber, TelephoneNumber aFaxNumber) {
+        addressInformation = anAddressInformation;
         emailAddress = anEmailAddress;
         telephoneNumber = aTelephoneNumber;
         faxNumber = aFaxNumber;
@@ -23,9 +27,20 @@ public class Contact implements Value<Contact> {
 
     }
 
+    public static Contact of(AddressInformation anAddressInformation, EmailAddress anEmailAddress, TelephoneNumber aTelephoneNumber, TelephoneNumber aFaxNumber) {
+        return new Contact(anAddressInformation, anEmailAddress, aTelephoneNumber, aFaxNumber);
+
+    }
+
+    public static ContactBuilder builder() {
+        return new ContactBuilder();
+
+    }
+
     @Override
     public boolean sameValueAs(Contact other) {
         return equals(other);
+
     }
 
     @Override
@@ -33,7 +48,7 @@ public class Contact implements Value<Contact> {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Contact contact = (Contact) o;
-        return Objects2.equal(address, contact.address) &&
+        return Objects2.equal(addressInformation, contact.addressInformation) &&
                 Objects2.equal(emailAddress, contact.emailAddress) &&
                 Objects2.equal(telephoneNumber, contact.telephoneNumber) &&
                 Objects2.equal(faxNumber, contact.faxNumber);
@@ -42,7 +57,7 @@ public class Contact implements Value<Contact> {
 
     @Override
     public int hashCode() {
-        return Objects2.hash(address, emailAddress, telephoneNumber, faxNumber);
+        return Objects2.hash(addressInformation, emailAddress, telephoneNumber, faxNumber);
 
     }
 
@@ -52,9 +67,77 @@ public class Contact implements Value<Contact> {
 
     @Override
     public boolean isEmpty() {
-        return address.isEmpty()
+        return addressInformation.isEmpty()
                 && emailAddress.isEmpty()
                 && telephoneNumber.isEmpty()
                 && faxNumber.isEmpty();
     }
+
+    @Override
+    public String toString() {
+        return toStringHelper(this)
+                .defaultConfig()
+                .toString();
+    }
+
+    public static class ContactBuilder extends ObjectBuilder<Contact, ContactBuilder> {
+        private AddressInformation addressInformation = AddressInformation.empty();
+        private EmailAddress emailAddress = EmailAddress.empty();
+        private TelephoneNumber telephoneNumber = TelephoneNumber.empty();
+        private TelephoneNumber faxNumber = TelephoneNumber.empty();
+
+        @Override
+        protected void apply(Contact vo, ContactBuilder builder) {
+            builder.withAddressInformation(vo.addressInformation);
+            builder.withEmailAddress(vo.emailAddress);
+            builder.withTelephoneNumber(vo.telephoneNumber);
+            builder.withFaxNumber(vo.faxNumber);
+
+
+        }
+
+        public ContactBuilder withFaxNumber(TelephoneNumber faxNumber) {
+            if (isNull(faxNumber)) return getThis();
+            addConfigurator(builder -> builder.faxNumber = faxNumber);
+            return getThis();
+
+        }
+
+        public ContactBuilder withTelephoneNumber(TelephoneNumber telephoneNumber) {
+            if (isNull(telephoneNumber)) return getThis();
+            addConfigurator(builder -> builder.telephoneNumber = telephoneNumber);
+            return getThis();
+
+        }
+
+        public ContactBuilder withEmailAddress(EmailAddress emailAddress) {
+            if (isNull(emailAddress)) return getThis();
+            addConfigurator(builder -> builder.emailAddress = emailAddress);
+            return getThis();
+
+        }
+
+        public ContactBuilder withAddressInformation(AddressInformation addressInformation) {
+            if (isNull(addressInformation)) return getThis();
+            addConfigurator(builder -> builder.addressInformation = addressInformation);
+            return getThis();
+        }
+
+        @Override
+        protected Contact createValueObject() {
+            return new Contact(addressInformation, emailAddress, telephoneNumber, faxNumber);
+        }
+
+        @Override
+        protected ContactBuilder getThis() {
+            return this;
+        }
+
+        @Override
+        protected ContactBuilder newInstance() {
+            return new ContactBuilder();
+        }
+    }
+
+
 }
---
+++ src/test/java/org/venuspj/ddd/model/values/buisiness/contact/ContactTest.java
@@ -0,0 +1,134 @@
+package org.venuspj.ddd.model.values.buisiness.contact;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.venuspj.ddd.model.values.buisiness.address.AddressInformation;
+import org.venuspj.ddd.model.values.buisiness.address.SimpleAddressInformation;
+import org.venuspj.ddd.model.values.buisiness.contact.email.EmailAddress;
+import org.venuspj.ddd.model.values.buisiness.contact.telephone.TelephoneNumber;
+import org.venuspj.tests.constants.TestSize;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ContactTest {
+    @Test
+    @Tag(TestSize.SMALL)
+    public void empty() {
+        Contact target = Contact.empty();
+
+        boolean actual = target.isEmpty();
+
+        assertThat(actual)
+                .isTrue();
+
+    }
+
+    @Test
+    @Tag(TestSize.SMALL)
+    public void of() {
+        Contact target = Contact.of(AddressInformation.empty(),
+                EmailAddress.empty(), TelephoneNumber.empty(), TelephoneNumber.empty());
+
+        boolean actual = target.isEmpty();
+
+        assertThat(actual)
+                .isTrue();
+
+    }
+
+    @Test
+    @Tag(TestSize.SMALL)
+    public void builder1() {
+        Contact target = Contact
+                .builder()
+                .withAddressInformation(AddressInformation.empty())
+                .withEmailAddress(EmailAddress.empty())
+                .withTelephoneNumber(TelephoneNumber.empty())
+                .withFaxNumber(TelephoneNumber.empty())
+                .build();
+
+        boolean actual = target.isEmpty();
+
+        assertThat(actual)
+                .isTrue();
+
+    }
+
+    @Test
+    @Tag(TestSize.SMALL)
+    public void builder2() {
+        Contact target = Contact
+                .builder()
+                .withAddressInformation(null)
+                .withEmailAddress(null)
+                .withTelephoneNumber(null)
+                .withFaxNumber(null)
+                .build();
+
+        boolean actual = target.isEmpty();
+
+        assertThat(actual)
+                .isTrue();
+
+    }
+
+    @Test
+    @Tag(TestSize.SMALL)
+    public void sameValueAs1() {
+        Contact target = Contact.empty();
+
+        boolean actual = target.sameValueAs(target);
+
+        assertThat(actual)
+                .isTrue();
+
+    }
+
+    @Test
+    @Tag(TestSize.SMALL)
+    public void sameValueAs2() {
+        Contact target = Contact.empty();
+
+        boolean actual = target.sameValueAs(null);
+
+        assertThat(actual)
+                .isFalse();
+
+    }
+
+    @Test
+    @Tag(TestSize.SMALL)
+    public void sameValueAs3() {
+        Contact arg = Contact.empty();
+        Contact target = Contact.empty();
+
+        boolean actual = target.sameValueAs(arg);
+
+        assertThat(actual)
+                .isTrue();
+
+    }
+
+    @Test
+    @Tag(TestSize.SMALL)
+    public void toString1() {
+        Contact target = Contact.empty();
+
+        System.out.println(target.toString());
+
+    }
+
+    @Test
+    @Tag(TestSize.SMALL)
+    public void equals1() {
+        SimpleAddressInformation arg = SimpleAddressInformation.empty();
+        Contact target = Contact.empty();
+        boolean actual = target.equals(arg);
+        assertThat(actual)
+                .isFalse();
+
+        System.out.println(target.toString());
+
+    }
+
+}
