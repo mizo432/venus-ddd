79897b47d940d90bc110d26891ee2f58717084e1
溝口顕一 <hachirouuemon.mizoguchi@gmail.com>
2020-02-05 22:17:31 +0900

    testを追加した

--- src/main/java/org/venuspj/ddd/model/values/buisiness/address/AzaInformation.java
+++ src/main/java/org/venuspj/ddd/model/values/buisiness/address/AzaInformation.java
@@ -2,23 +2,62 @@ package org.venuspj.ddd.model.values.buisiness.address;
 
 import org.venuspj.ddd.model.values.Value;
 import org.venuspj.ddd.model.values.buisiness.Name;
+import org.venuspj.util.objects2.Objects2;
+
+import static org.venuspj.util.objects2.Objects2.toStringHelper;
 
 public class AzaInformation implements Value<AzaInformation> {
     private Name name = Name.empty();
 
+    AzaInformation(Name aName) {
+        name = aName;
+
+    }
+
+    public AzaInformation() {
+
+    }
+
     public static AzaInformation empty() {
         return new AzaInformation();
+
+    }
+
+    public static AzaInformation of(Name aName) {
+        return new AzaInformation(aName);
     }
 
 
     @Override
     public boolean sameValueAs(AzaInformation other) {
-        return false;
+        return equals(other);
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AzaInformation that = (AzaInformation) o;
+        return Objects2.equal(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects2.hash(name);
     }
 
     @Override
     public boolean isEmpty() {
         return name.isEmpty();
+
     }
 
+    @Override
+    public String toString() {
+        return toStringHelper(this)
+                .defaultConfig()
+                .toString();
+
+    }
 }
--- src/main/java/org/venuspj/ddd/model/values/buisiness/address/CityInformation.java
+++ src/main/java/org/venuspj/ddd/model/values/buisiness/address/CityInformation.java
@@ -2,21 +2,51 @@ package org.venuspj.ddd.model.values.buisiness.address;
 
 import org.venuspj.ddd.model.values.Value;
 import org.venuspj.ddd.model.values.buisiness.Name;
+import org.venuspj.util.objects2.Objects2;
+
+import static org.venuspj.util.objects2.Objects2.toStringHelper;
 
 public class CityInformation implements Value<CityInformation> {
     private Name name = Name.empty();
 
     public static CityInformation empty() {
         return new CityInformation();
+
     }
 
     @Override
     public boolean sameValueAs(CityInformation other) {
-        return false;
+        return equals(other);
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CityInformation that = (CityInformation) o;
+        return Objects2.equal(name, that.name);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects2.hash(name);
+
     }
 
     @Override
     public boolean isEmpty() {
         return name.isEmpty();
+
     }
+
+    @Override
+    public String toString() {
+        return toStringHelper(this)
+                .defaultConfig()
+                .toString();
+
+    }
+
 }
