8cf9993b1f1e702ce55bcc6b1bac90c467d76d1c
溝口顕一 <hachirouuemon.mizoguchi@gmail.com>
2020-02-16 03:02:50 +0900

    Historyを集約した

--- src/main/java/org/venuspj/basic/model/domain/relashonshipManagement/party/UserStaticsHistoryYear.java
+++ src/main/java/org/venuspj/basic/model/domain/relashonshipManagement/party/UserStaticsHistoryYear.java
@@ -1,6 +1,8 @@
 package org.venuspj.basic.model.domain.relashonshipManagement.party;
 
-import org.venuspj.ddd.model.values.buisiness.datetime.AbstractYearMonthHistory;
+import org.venuspj.ddd.model.values.buisiness.datetime.AbstractHistory;
 
-public class UserStaticsHistoryYear extends AbstractYearMonthHistory<UserStatics, UserStaticsHistoryYear> {
+import java.time.Year;
+
+public class UserStaticsHistoryYear extends AbstractHistory<UserStatics, UserStaticsHistoryYear, Year> {
 }
