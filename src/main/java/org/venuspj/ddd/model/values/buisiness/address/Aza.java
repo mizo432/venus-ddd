f2e1aab628f8ead0eafc22cd203a0aa97f9d4c46
溝口顕一 <hachirouuemon.mizoguchi@gmail.com>
2020-02-17 23:10:29 +0900

    debug Aza

--- src/main/java/org/venuspj/ddd/model/values/buisiness/address/Aza.java
+++ src/main/java/org/venuspj/ddd/model/values/buisiness/address/Aza.java
@@ -1,26 +1,16 @@
 package org.venuspj.ddd.model.values.buisiness.address;
 
-import org.venuspj.ddd.model.entity.AbstractEntity;
 import org.venuspj.ddd.model.values.buisiness.name.Name;
 
-import static org.venuspj.util.objects2.Objects2.equal;
-import static org.venuspj.util.objects2.Objects2.hash;
-
-public class Aza extends AbstractEntity<Aza, AzaIdentifier> {
-    private AzaCode azaCode = AzaCode.empty();
-    private Name name = Name.empty();
-    private Name kanaName = Name.empty();
+public class Aza extends AbstractAza<Aza> {
 
     Aza(AzaIdentifier anAzaIdentifier, AzaCode anAzaCode, Name aName, Name aKanaName) {
-        super(anAzaIdentifier);
-        azaCode = anAzaCode;
-        name = aName;
-        kanaName = aKanaName;
+        super(anAzaIdentifier, anAzaCode, aName, aKanaName);
 
     }
 
     public Aza() {
-        super(AzaIdentifier.empty());
+        super();
     }
 
     public static Aza empty() {
@@ -33,19 +23,26 @@ public class Aza extends AbstractEntity<Aza, AzaIdentifier> {
 
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
-        Aza aza = (Aza) o;
-        return equal(azaCode, aza.azaCode) &&
-                equal(name, aza.name) &&
-                equal(kanaName, aza.kanaName);
+    public static AzaBuilder builder() {
+        return new AzaBuilder();
     }
 
-    @Override
-    public int hashCode() {
-        return hash(super.hashCode(), azaCode, name, kanaName);
+    public static class AzaBuilder extends AbstractAzaBuilder<Aza, AzaBuilder> {
+
+        @Override
+        protected Aza createValueObject() {
+            return new Aza(super.identifier, super.azaCode, super.name, super.kanaName);
+        }
+
+        @Override
+        protected AzaBuilder getThis() {
+            return this;
+        }
+
+        @Override
+        protected AzaBuilder newInstance() {
+            return new AzaBuilder();
+        }
     }
+
 }
--- src/test/java/org/venuspj/ddd/model/values/buisiness/address/AzaTest.java
+++ src/test/java/org/venuspj/ddd/model/values/buisiness/address/AzaTest.java
@@ -31,4 +31,121 @@ public class AzaTest {
 
         System.out.println(true);
     }
+
+    @Test
+    @Tag(TestSize.SMALL)
+    public void sameValueAs1() {
+        Aza target = AzaMock.one();
+        assertThat(target.sameValueAs(AzaMock.one()))
+                .isTrue();
+
+    }
+
+    @Test
+    @Tag(TestSize.SMALL)
+    public void sameValueAs2() {
+        Aza target = AzaMock.one();
+        assertThat(target.sameValueAs(target))
+                .isTrue();
+
+    }
+
+    @Test
+    @Tag(TestSize.SMALL)
+    public void sameValueAs3() {
+        Aza target = AzaMock.one();
+        assertThat(target.sameValueAs(null))
+                .isFalse();
+
+    }
+
+    @Test
+    @Tag(TestSize.SMALL)
+    public void sameValueAs4() {
+        Aza target = AzaMock.one();
+        assertThat(target.sameValueAs(AzaMock.two()))
+                .isFalse();
+
+    }
+
+    @Test
+    @Tag(TestSize.SMALL)
+    public void sameIdentifierAs1() {
+        Aza target = AzaMock.one();
+        assertThat(target.sameIdentifierAs(AzaMock.one()))
+                .isTrue();
+
+    }
+
+    @Test
+    @Tag(TestSize.SMALL)
+    public void sameIdentifierAs2() {
+        Aza target = AzaMock.one();
+        assertThat(target.sameIdentifierAs(target))
+                .isTrue();
+
+    }
+
+    @Test
+    @Tag(TestSize.SMALL)
+    public void sameIdentifierAs3() {
+        Aza target = AzaMock.one();
+        assertThat(target.sameIdentifierAs(null))
+                .isFalse();
+
+    }
+
+    @Test
+    @Tag(TestSize.SMALL)
+    public void sameIdentifierAs4() {
+        Aza target = AzaMock.one();
+        assertThat(target.sameIdentifierAs(AzaMock.two()))
+                .isFalse();
+
+    }
+
+    @Test
+    @Tag(TestSize.SMALL)
+    public void sameIdentifierAs5() {
+        Aza target = AzaMock.one();
+        assertThat(target.sameIdentifierAs(AzaMock.sameIdentifier()))
+                .isTrue();
+
+    }
+
+    public static class AzaMock {
+        Aza empty() {
+            return Aza.empty();
+        }
+
+        static Aza one() {
+            return Aza
+                    .builder()
+                    .withIdentifier(AzaIdentifier.of(1L))
+                    .withAzaCode(AzaCode.of("001"))
+                    .withName(Name.of("１丁目"))
+                    .withKanaName(Name.of("１ッチョウメ"))
+                    .build();
+        }
+
+        static Aza two() {
+            return Aza
+                    .builder()
+                    .withIdentifier(AzaIdentifier.of(2L))
+                    .withAzaCode(AzaCode.of("001"))
+                    .withName(Name.of("１丁目"))
+                    .withKanaName(Name.of("１ッチョウメ"))
+                    .build();
+        }
+
+        static Aza sameIdentifier() {
+            return Aza
+                    .builder()
+                    .withIdentifier(AzaIdentifier.of(1L))
+                    .withAzaCode(AzaCode.of("001-1"))
+                    .withName(Name.of("１丁目-1"))
+                    .withKanaName(Name.of("１ッチョウメ-1"))
+                    .build();
+        }
+    }
 }
